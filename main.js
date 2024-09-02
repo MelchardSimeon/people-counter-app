f60c41a84c80e3f68b5c4c91aa2752db4d82f322
MelchardSimeon <melchardsimeon71@gmail.com>
2024-09-02 22:01:56 +0800

    add save button

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 
 let counter = 0;
 let countEl = document.getElementById("count-el");
-
+let saveEl = document.getElementById("save-el");
 
 function increment() {
     counter += 1;
     countEl.innerText = counter;
     console.log(counter)
+}
+
+function save() {
+    console.log(counter);
 }
